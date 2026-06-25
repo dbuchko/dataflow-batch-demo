f5f6c1550487bc40d74158fda2f5525bd84b105b
dbuchko <dbuchko@vmware.com>
2026-06-25 16:53:43 -0400

    Initial framework from Spring Initializer

---
+++ src/main/java/com/demo/dfbatch/DfbatchApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dfbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/dfbatch/DfbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.dfbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
